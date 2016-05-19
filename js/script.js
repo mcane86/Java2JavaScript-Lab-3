a8f54c0cd5b670746a002b7060c46bf7b8e47b16
Michael Cane <michaelcane@Zipcoder06s-MacBook-Pro.local>
2016-05-19 09:51:08 -0400

    Committing to GitHub

--- js/script.js
+++ js/script.js
@@ -1,28 +1,75 @@
 " use strict ";
 
-function PhoneBook(){
+var contactList = {};
 
+function PhoneBook(){
+	
 };
 
 function listAllNames() {
-  // code to list all names
+	var listName = "";
+	for(var name in contactList) {
+		listName += name + "<br/>";
+	}
+	display.innerHTML = listName;
 };
 
 function listAllNumbers() {
-  // code to list all numbers
+	var listNums = "";
+	for(var name in contactList) {
+		listNums += contactList[name] + "<br/>";
+	}
+	display.innerHTML = listNums;
 };
+
+function listAll() {
+	var listThem = "";
+	for(var name in contactList) {
+		listThem += name + ": " + contactList[name]+ "<br/>";
+	}
+	display.innerHTML = listThem;
+}
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  contactList[name] = [number];
 };
 
+function showAddAdditional() {
+	var name = prompt("Enter name");
+	var number = prompt("Enter number to add");
+	contactList[name].push(" " + number);
+}
+
 function showRemove() {
-  var remove = prompt("Enter name to remove");
+	var name = prompt("Enter name");
+	var number = prompt("Enter number to remove");
+	var toRemove = contactList[name].indexOf(number);
+	contactList[name].splice(toRemove, toRemove+1);
+};
+
+function showRemoveRecord() {
+	var remove = prompt("Enter name to remove");
+	delete contactList[remove];
 };
 
 function showLookup() {
-  var lookup = prompt("Enter number to lookup");
+	var lookupByName = prompt("Enter name to lookup");
+  	for(i in contactList) {
+  		if(lookupByName == i) {
+  			display.innerHTML = contactList[i];
+  		}
+  	}
+};
+
+function reverseLookup() {
+	var lookupByNumber = prompt("Enter name to lookup");
+  	for(i in contactList) {
+  		if(lookupByNumber == contactList[i]) {
+  			display.innerHTML = i;
+  		}
+  	}
 };
 
 var display = document.getElementById("display");
